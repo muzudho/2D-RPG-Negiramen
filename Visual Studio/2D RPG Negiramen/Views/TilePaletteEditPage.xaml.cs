e2199356ba49e9cf17325b959216e254d36c39fc
むずでょ <muzudho1@gmail.com>
2023-07-09 00:45:50 +0900

    Selected Tile Form Display

--- Visual Studio/2D RPG Negiramen/Views/TilePaletteEditPage.xaml.cs
+++ Visual Studio/2D RPG Negiramen/Views/TilePaletteEditPage.xaml.cs
@@ -64,25 +64,14 @@ public partial class TilePaletteEditPage : ContentPage
             //
             TheDiagnostics.Trace.WriteLine("[TilePaletteEditPage.xml.cs TapGestureRecognizer_Tapped] 疑似マウス・ダウン");
 
-            // タップした位置
+            // ポイントしている位置
             PointingDeviceCurrentPoint = PointingDeviceStartPoint = new Models.Point(
                 new Models.X((int)e.GetPosition((Element)sender).Value.X),
                 new Models.Y((int)e.GetPosition((Element)sender).Value.Y));
             // Trace.WriteLine($"[TilePaletteEditPage TapGestureRecognizer_Tapped] tapped x:{PointingDeviceStartPoint.X.AsInt} y:{PointingDeviceStartPoint.Y.AsInt}");
 
-            // タイル・カーソルの矩形
-            var cursorRectangle = Models.CoordinateHelper.GetCursorRectangle(PointingDeviceStartPoint, PointingDeviceCurrentPoint);
-            // Trace.WriteLine($"[TilePaletteEditPage TapGestureRecognizer_Tapped] cursorRectangle x:{cursorRectangle.Point.X.AsInt} y:{cursorRectangle.Point.Y.AsInt} width:{cursorRectangle.Size.Width.AsInt} height:{cursorRectangle.Size.Height.AsInt}");
-
-            //
-            // 計算値の反映
-            // ============
-            //
-            TilePaletteEditPageViewModel context = (TilePaletteEditPageViewModel)this.BindingContext;
-            context.SelectedTileLeftAsInt = cursorRectangle.Point.X.AsInt;
-            context.SelectedTileTopAsInt = cursorRectangle.Point.Y.AsInt;
-            context.SelectedTileWidthAsInt = cursorRectangle.Size.Width.AsInt;
-            context.SelectedTileHeightAsInt = cursorRectangle.Size.Height.AsInt;
+            // タイル・フォームの表示更新
+            RefreshTileForm();
         }
         else
         {
@@ -126,20 +115,6 @@ public partial class TilePaletteEditPage : ContentPage
 
             // タイル・フォームの表示更新
             RefreshTileForm();
-
-            //// ポインティング・デバイスの２箇所のタップ位置から、タイルの矩形を算出
-            //var selectingTileRectangle = Models.CoordinateHelper.GetCursorRectangle(PointingDeviceStartPoint, PointingDeviceCurrentPoint);
-            //// Trace.WriteLine($"[TilePaletteEditPage PointerGestureRecognizer_PointerMoved] cursorRectangle x:{cursorRectangle.Point.X.AsInt} y:{cursorRectangle.Point.Y.AsInt} width:{cursorRectangle.Size.Width.AsInt} height:{cursorRectangle.Size.Height.AsInt}");
-
-            ////
-            //// 計算値の反映
-            //// ============
-            ////
-            //TilePaletteEditPageViewModel context = (TilePaletteEditPageViewModel)this.BindingContext;
-            //context.SelectedTileLeftAsInt = selectingTileRectangle.Point.X.AsInt;
-            //context.SelectedTileTopAsInt = selectingTileRectangle.Point.Y.AsInt;
-            //context.SelectedTileWidthAsInt = selectingTileRectangle.Size.Width.AsInt;
-            //context.SelectedTileHeightAsInt = selectingTileRectangle.Size.Height.AsInt;
         }
     }
 
