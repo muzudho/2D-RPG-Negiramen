9cf1cdcbb83774e06456cfde177d4f5ecc9d6b18
むずでょ <muzudho1@gmail.com>
2023-07-29 12:17:29 +0900

    Login Page

--- Visual Studio/2D RPG Negiramen/Views/LoginPage2.xaml.cs
+++ Visual Studio/2D RPG Negiramen/Views/LoginPage2.xaml.cs
@@ -6,4 +6,34 @@ public partial class Login2Page : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    // - プライベート・イベントハンドラ
+
+    #region イベントハンドラ（ロケール変更時）
+    /// <summary>
+    ///     ロケール変更時
+    /// </summary>
+    /// <param name="sender">このイベントを呼び出したコントロール</param>
+    /// <param name="e">この発生イベントの制御変数</param>
+    void LocalePicker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        // ＸＡＭＬではなく、Ｃ＃で動的に翻訳を行っている場合のための変更通知
+        //var context = this.TileCropPageVM;
+        //context.InvalidateLocale();
+    }
+    #endregion
+
+    #region イベントハンドラ（［ホーム］ボタン・クリック時）
+    /// <summary>
+    ///     ［ホーム］ボタン・クリック時
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    async void HomeBtn_Clicked(object sender, EventArgs e)
+    {
+        await PolicyOfView.ReactOnPushed((Button)sender);
+
+        await Shell.Current.GoToAsync("//MainPage");
+    }
+    #endregion
 }
